40d8523e24a5e6fecad54ea0037257040311161b
Paja777 <pavle.rvovic95@gmail.com>
2024-03-04 22:09:29 +0100

    Error boundries

--- controllers/adControllers.js
+++ controllers/adControllers.js
@@ -30,7 +30,7 @@ const getAd = async (req, res) => {
   const { id } = req.params;
 
   if (!mongoose.Types.ObjectId.isValid(id)) {
-    return res.status(404).json({ error: "No such workout" });
+    return res.status(404).json({ error: "No such ad" });
   }
   const ad = await Ad.findById(id);
 
