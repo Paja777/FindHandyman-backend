6deafc812648d4880a3718d230f62c02300892f1
Paja777 <pavle.rvovic95@gmail.com>
2024-03-24 14:17:56 +0100

    corse middleware recofig

--- server.js
+++ server.js
@@ -9,6 +9,27 @@ const mongoose = require('mongoose');
 
 const app = express();
 
+app.use((req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://find-handyman.onrender.com/"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Private-Network", true);
+  // Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
+  res.setHeader("Access-Control-Max-Age", 7200);
+
+  next();
+});
+
 // Increase payload size limit (e.g., 10MB)
 app.use(bodyParser.json({ limit: '10mb' }));
 app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
@@ -19,11 +40,11 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use(cors({
-    origin: 'https://find-handyman.onrender.com/',
-    credentials: true 
+// app.use(cors({
+//     origin: 'https://find-handyman.onrender.com/',
+//     credentials: true 
 
-}));
+// }));
 
 app.use("/ad", adsRoutes);
 app.use("/user", userRoutes);
